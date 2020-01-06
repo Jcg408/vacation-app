64f680bb480a82c805aeb703743c4b9409465573
Jcg408 <jcg321@comcast.net>
2020-01-06 14:55:54 -0500

    rename css file to scss, add partial to layouts nav, update view to render nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
-         has_many: properties
+         has_many  :properties
 end
