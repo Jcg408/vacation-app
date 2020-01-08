23613b5246ef654a80e0fe85892c1a3e8cb4277c
Jcg408 <jcg321@comcast.net>
2020-01-08 16:00:45 -0500

    add permitted parameters to devise session and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-    
+    before_action :config_permitted_params, if: :devise_controller?
+
+    protected
+
+    def config_permitted_params
+        devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :email, :password, :url])
+        devise_parameter_sanitizer.permit(:update, keys:[:first_name, :last_name, :email, :password, :url])
+    end
 
 end
